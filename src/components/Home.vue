a4e49b74bbd806b260c41135ea1b958619a54187
김현성 <whitekim88@gmail.com>
2019-10-24 17:59:43 +0900

    project init

---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    Home
+    <div>
+      Board List:
+      <div v-if="loading">
+        Loading...
+      </div>
+      <div v-else>
+        Api Result : {{apiRes}}
+      </div>
+      <ul>
+        <li>
+          <router-link to="/b/1">board1</router-link>
+        </li>
+        <li>
+          <router-link to="/b/2">board2</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: false,
+      apiRes: ''
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+
+      const req = new XMLHttpRequest()
+      req.open('GET', 'http://localhost:3000/health')
+      req.send()
+      req.addEventListener('load', () => {
+        this.loading = false
+        this.apiRes = {
+          status: req.status,
+          statusText: req.statusText,
+          response: JSON.parse(req.response)
+        }
+      })
+    }
+  },
+}
+</script>
